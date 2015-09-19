c3dcaf10074f7d035344b9b1fe78d7fb1043d577
RFoerster <robert.jun@foerster.co.at>
2015-09-19 08:49:45 +0200

    First Commit

---
+++ GeometricObjectsSolution/Circle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjectsSolution
+{
+    public class Circle
+    {
+        public double XCoordinate {get; set; }
+        public double YCoordinate { get; set; }
+        private int _Radius;
+
+        public int Radius
+        {
+            get { return _Radius; }
+            set
+            {
+                if (value >= 0)
+                    _Radius = value;
+                else
+                    Console.WriteLine("Ungültiger negativer Wert für Radius.");
+            }
+        }
+
+        public double GetArea()
+        {
+            double area = Math.Pow(Radius, 2) * Math.PI;
+            return area;
+        }
+
+        public double GetCircumference()
+        {
+            double circumference = 2 * Radius * Math.PI;
+            return circumference;
+        }
+
+        public void Move(double dx, double dy)
+        {
+            XCoordinate += dx;
+            YCoordinate += dy;
+        }
+
+        public int Bigger(Circle kreis)
+        {
+            if (Radius > kreis.Radius) return 1;
+            if (Radius < kreis.Radius) return -1;
+            else return 0;
+        }
+
+
+      
+    }
+}
---
+++ GeometricObjectsSolution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjectsSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle Kreis = new Circle();
+            Kreis.Radius = -100;
+            Kreis.XCoordinate = 1.5;
+            Kreis.YCoordinate = -4.5;
+            Console.WriteLine("Der Radius des Kreises ist {0}, die XCoordinate ist {1} und die YCoordinate ist {2}", Kreis.Radius, Kreis.XCoordinate, Kreis.YCoordinate);
+            Kreis.Move(20, 40);
+            Console.WriteLine("Der Radius des Kreises ist {0}, die XCoordinate ist {1} und die YCoordinate ist {2}", Kreis.Radius, Kreis.XCoordinate, Kreis.YCoordinate);
+
+            Console.ReadLine();
+        }
+    }
+}
