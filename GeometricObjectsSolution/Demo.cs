4848aac573da4eb740bf3cc820a63d85ee09b982
RFoerster <robert.jun@foerster.co.at>
2015-09-20 23:01:02 +0200

    Änderungen

---
+++ GeometricObjectsSolution/Demo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjectsSolution
+{
+    class Demo
+    {
+        public static double AddValues(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double SubstractValues(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static void ShoutOut()
+        {
+            Console.WriteLine("ShoutOutLoud");
+        }
+
+        public static void Whisper()
+        {
+            Console.WriteLine("Whisper");
+        }
+    }
+}
