e0842059059a61624ed33335800aa588d829672b
RFoerster <robert.jun@foerster.co.at>
2015-09-22 18:54:23 +0200

    neu

--- GeometricObjectsSolution/Program.cs
+++ GeometricObjectsSolution/Program.cs
@@ -1,39 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.IO;
+using System.Diagnostics;   
 
 namespace GeometricObjectsSolution
 {
-    public delegate double CalculateHandler(double value1, double value2);
-    public delegate void SayHiHandler();
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            GraphicCircle kreis1 = new GraphicCircle(-10, 5, 4);
-            kreis1.InvalidMeasure += kreis_InvalidMeasure;
-            Console.WriteLine(kreis1.ToString());
+            ArrayList liste = new ArrayList { "Robert", "Markus", "Papa", "Ines" };
+            Console.WriteLine("-------------Schreibe Liste-------------");
+            PrintListe(liste);
+            Console.WriteLine("Liste geschrieben \n\n");
+            liste.Remove("Robert");
+            Console.WriteLine("-------------Schreibe neue Liste --------------");
+            PrintListe(liste);
             Console.ReadLine();
-            kreis1.Radius = -1;
-            Console.WriteLine(kreis1.ToString());
-            Console.ReadLine();
-            
-                
+
         }
 
-        private static void Kreis1_InvalidMeasure()
+        static void PrintListe(IList liste)
         {
-            Console.WriteLine("Du bist dumm");
-        }
-
-        public static void kreis_InvalidMeasure(Circle sender, InvalidMeasureEventArgs e)
-        {
-            Console.Write("Unzulässiger negativer Radius ({0}), bitte neuen Radius für {1} eingeben:", e.InvalidMeasure, e.PropertyName);
-            sender.Radius = Convert.ToInt32(Console.ReadLine());
+            foreach (string item in liste)
+                Console.WriteLine("Index: {0,-3} {1}", liste.IndexOf(item), item);
         }
     }
 }
